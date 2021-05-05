be050006791ac832b4943f65fcbdaeeb5bc72c34
LIRUILONGS <1224965096@qq.com>
2021-05-05 09:16:05 +0800

    Vue中markdown使用katex(基于mavonEditor扩展katex）

---
+++ src/components/demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+     <mavon-editor v-model="value"/>
+     <div ref="katex" v-katex="'\\frac{a_i}{1+x}'"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  props: {
+  },
+  data(){
+    return{
+      value:this.$refs.katex.textContent,
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'mavon-editor/dist/css/index.css'
+import mavonEditor from 'mavon-editor'
+//import VueKatex from 'vue-katex';
+//import 'katex/dist/katex.min.css';
+import Md_Katex from '@iktakahiro/markdown-it-katex'
 
-Vue.config.productionTip = false
 
+
+//Vue.use(VueKatex)
+//Vue.config.productionTip = false
+Vue.use(mavonEditor)
+
+
+mavonEditor.markdownIt.set({}).use(Md_Katex);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
